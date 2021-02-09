68e6f66df60503e41534494d2f1ab60d7c72264f
michel memeteau <mm@ekimia.fr>
2021-02-10 00:35:38 +0100

    Update synchome2usb.sh

--- synchome2usb.sh
+++ synchome2usb.sh
@@ -6,15 +6,34 @@
 
 # SYnc ALL 
 
-disk1=0FF6-072F
+#disk1=0FF6-072F
 disk2=7E90-3EC9
 disk3=1CE3-3215
 
+disk1=85B4-3D98
+
 #Detect what disk is connected
+blkline=$(blkid |grep $disk1)
+echo $blkline
+#Testing if blkline is empty 
+if test -z "$blkline"
+then
+    echo "Disque 1 is not connected"
+else 
+   echo "Disque 1 is connected ,finding device ..."
+   device=$(echo $blkline | cut -c 1-9)
+   echo "Disque 1 is connected on $device"
+   mountline=$(mount |grep $device)
+   echo $mountline
+   if test -z "$mountline"
+   then 
+    echo " Disque 1 is not mounted "
+   else 
+    echo " Disque 1 is  mounted  on ...."
+   fi 
+fi
 
 
+#Start Rsync on disque 1
 
-
-#Start Rsync
-
-rsync -rt --stats /home/. /media/sdd/ > /root/syncusb.txt
+#rsync -rt --stats /home/. /media/sdd/ > /root/syncusb.txt
