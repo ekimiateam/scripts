4e35293e7d12117fb88a8eae8e1848e2b626b4c1
Michel Memeteau <mm@ekimia.fr>
2021-07-09 09:55:28 +0200

    Structure

--- enablehibernate.sh
+++ enablehibernate.sh
@@ -1,3 +1,29 @@
 #/bin/bash
-# Ekimia.fr
-# Enables Hibernation with menus on Ubuntu 20.04
+# Ekimia.fr 2021 
+# Enables Hibernation with swap file with menus on Ubuntu 20.04
+
+
+echo " starting enabling hibernate "
+
+# install needed packages 
+
+
+# Compute ideal size of swap ( Memesize * 1.5 ) 
+
+
+# Get UUID & swap_offset 
+
+
+# Modify initramfs 
+
+
+
+# Update initramfs 
+
+sudo update-initramfs -k all -u
+
+
+# update polkit 
+
+
+# 
